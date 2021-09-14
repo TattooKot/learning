7fa89e224d560fb7e7c323be80dbd4ee5009fa42
TattooKot <kotsagastas@gmail.com>
2021-09-14 15:11:06 +0300

    Chapter 18: ArrayDeque listing;

---
+++ src/main/java/com/tattookot/javacore/chapter18/ArrayDequeDemo.java
@@ -0,0 +1,21 @@
+package main.java.com.tattookot.javacore.chapter18;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeDemo {
+    public static void main(String[] args) {
+
+        ArrayDeque<String> adq = new ArrayDeque<>();
+
+        adq.push("A");
+        adq.push("B");
+        adq.push("D");
+        adq.push("E");
+        adq.push("F");
+
+        System.out.print("Stack: ");
+
+        while(adq.peek() != null)
+            System.out.print(adq.pop() + " ");
+    }
+}
